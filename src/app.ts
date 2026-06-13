8922e4271ba3ea478a594966bbb60f218fb4a497
KenDev2105 <195981080+KenDev2105@users.noreply.github.com>
2026-06-12 21:00:37 -0600

    Combine tema5 & master branches

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,17 @@
 import express from "express";
 import cors from "cors";
 
+
 import gameRoutes from "./routes/games.routes";
 import section6Routes from "./routes/section6.routes";
 import section6QuestionRoutes from "./routes/section6Question.routes";
 import section6ValidateRoutes from "./routes/section6Validate.routes";
 import section6StatsRoutes from "./routes/section6Stats.routes";
 import section3Routes from "./routes/section3.routes";
+import section5Routes from './routes/section5.routes';
+import section5QuestionRoutes from "./routes/section5Question.routes";
+import section5ValidateRoutes from "./routes/section5Validate.routes";
+import section5StatsRoutes from "./routes/section5Stats.routes";
 
 const app = express();
 app.use(cors());
@@ -16,6 +21,9 @@ app.use(express.json());
 
 // Routes
 app.use("/api/section3", section3Routes);
+app.use("/api/section5", section5Routes);
+app.use("/api/section5/questions", section5QuestionRoutes);
+app.use("/api/section5/stats", section5StatsRoutes);
 app.use("/api/section6/stats", section6StatsRoutes);
 
 app.use("/api/section6", section6Routes);
